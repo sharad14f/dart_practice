3d2f5ba77f2dd2ec717c57907b8c634735383d22
joker-king-sharad <58117725+joker-king-sharad@users.noreply.github.com>
2025-10-20 14:37:39 +0530

    1st practices

---
+++ 1.practice.dart
@@ -0,0 +1,18 @@
+void main(){
+    print("Hello, Dart!");
+    //Variables
+    int num= 60;
+    double decimalNum= 60.5;
+    String name= "Dart Programming";
+    bool isLearning= true;
+    var var_name = "This is a variable with inferred type";
+
+    // arthmetic operations
+
+    print("Addition: ${num + 10}");
+    print("Subtraction: ${num - 10}");
+    print("Multiplication: ${num * 2}");
+    print("Division: ${num / 2}");
+    print("Modulus: ${num % 7}");
+    
+}
